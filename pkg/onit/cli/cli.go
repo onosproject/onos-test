89583f8164f9d51ed4a7f0b3b3b103fb05189028
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2019-08-09 09:29:48 -0700

    Fix run test command to look for a test ID in the correct set of tests (#51)

--- pkg/onit/cli/cli.go
+++ pkg/onit/cli/cli.go
@@ -16,8 +16,9 @@ package cli
 
 import (
 	"fmt"
-	"github.com/onosproject/onos-test/pkg/runner"
 	"os"
+
+	"github.com/onosproject/onos-test/pkg/runner"
 
 	"github.com/google/uuid"
 	"github.com/onosproject/onos-test/pkg/onit/console"
@@ -32,6 +33,27 @@ func Contains(array []string, elem string) bool {
 		}
 	}
 	return false
+}
+
+// subset returns true if the first array is completely
+// contained in the second array.
+func Subset(first, second []string) bool {
+	set := make(map[string]int)
+	for _, value := range second {
+		set[value]++
+	}
+
+	for _, value := range first {
+		if count, found := set[value]; !found {
+			return false
+		} else if count < 1 {
+			return false
+		} else {
+			set[value] = count - 1
+		}
+	}
+
+	return true
 }
 
 // GetOnitCommand returns a Cobra command for tests in the given test registry
