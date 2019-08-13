d3f53b92287417a5c32a53d1af68c127f86bb464
Ray Milkey <ray@opennetworking.org>
2019-08-13 09:47:41 -0700

    Switched over to using a Deployment rather than a Pod. Creating and starting an app works, deete does not

--- pkg/onit/app.go
+++ pkg/onit/app.go
@@ -15,12 +15,12 @@
 package onit
 
 import (
-	"encoding/json"
-	"time"
-
+	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"time"
 )
 
 // GetSApps returns a list of apps deployed in the cluster
@@ -45,13 +45,13 @@ func (c *ClusterController) setupApp(name string, config *AppConfig) error {
 	if err := c.createAppConfigMap(name, config); err != nil {
 		return err
 	}
-	if err := c.createAppPod(name); err != nil {
-		return err
-	}
 	if err := c.createAppService(name); err != nil {
 		return err
 	}
-	if err := c.awaitAppReady(name); err != nil {
+	if err := c.createOnosAppDeployment(name); err != nil {
+		return err
+	}
+	if err := c.awaitOnosAppDeploymentReady(name); err != nil {
 		return err
 	}
 	return nil
@@ -59,86 +59,144 @@ func (c *ClusterController) setupApp(name string, config *AppConfig) error {
 
 // createAppConfigMap creates an app configuration
 func (c *ClusterController) createAppConfigMap(name string, config *AppConfig) error {
-	configObj, err := config.load()
-	if err != nil {
-		return err
-	}
-	configJSON, err := json.Marshal(configObj)
-	if err != nil {
-		return err
-	}
 	cm := &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: c.clusterID,
 		},
-		Data: map[string]string{
-			"config.json": string(configJSON),
-		},
+		Data: map[string]string{},
 	}
-	_, err = c.kubeclient.CoreV1().ConfigMaps(c.clusterID).Create(cm)
+	_, err := c.kubeclient.CoreV1().ConfigMaps(c.clusterID).Create(cm)
 	return err
 }
 
-// createAppPod creates an app pod
-func (c *ClusterController) createAppPod(name string) error {
-
-	pod := &corev1.Pod{
+// createOnosAppDeployment creates an app Deployment
+func (c *ClusterController) createOnosAppDeployment(name string) error {
+	nodes := int32(1) //int32(c.config.TopoNodes)
+	zero := int64(0)
+	dep := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: c.clusterID,
-			Labels: map[string]string{
-				"type": "app",
-				"app":  name,
-			},
 		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name: "app",
-					// TODO: pull in the image name from the command arguments
-					Image:           c.imageName("onosproject/onos-ztp", "latest"),
-					ImagePullPolicy: corev1.PullIfNotPresent,
-					Ports: []corev1.ContainerPort{
-						{
-							Name:          "gnmi",
-							ContainerPort: 5150,
-						},
-					},
-					ReadinessProbe: &corev1.Probe{
-						Handler: corev1.Handler{
-							TCPSocket: &corev1.TCPSocketAction{
-								Port: intstr.FromInt(5150),
-							},
-						},
-						InitialDelaySeconds: 5,
-						PeriodSeconds:       10,
-					},
-					LivenessProbe: &corev1.Probe{
-						Handler: corev1.Handler{
-							TCPSocket: &corev1.TCPSocketAction{
-								Port: intstr.FromInt(5150),
-							},
-						},
-						InitialDelaySeconds: 15,
-						PeriodSeconds:       20,
-					},
-					VolumeMounts: []corev1.VolumeMount{
-						{
-							Name:      "config",
-							MountPath: "/etc/app/configs",
-							ReadOnly:  true,
-						},
-					},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &nodes,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app":  "onos",
+					"type": "app",
 				},
 			},
-			Volumes: []corev1.Volume{
-				{
-					Name: "config",
-					VolumeSource: corev1.VolumeSource{
-						ConfigMap: &corev1.ConfigMapVolumeSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: name,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app":      "onos",
+						"type":     "app",
+						"resource": name,
+					},
+					Annotations: map[string]string{
+						"seccomp.security.alpha.kubernetes.io/pod": "unconfined",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:            name,
+							Image:           c.imageName("onosproject/onos-ztp", "latest"),
+							ImagePullPolicy: corev1.PullIfNotPresent,
+							Env: []corev1.EnvVar{
+								{
+									Name:  "ATOMIX_CONTROLLER",
+									Value: fmt.Sprintf("atomix-controller.%s.svc.cluster.local:5679", c.clusterID),
+								},
+								{
+									Name:  "ATOMIX_APP",
+									Value: "test",
+								},
+								{
+									Name:  "ATOMIX_NAMESPACE",
+									Value: c.clusterID,
+								},
+								{
+									Name:  "ATOMIX_RAFT_GROUP",
+									Value: "raft",
+								},
+							},
+							Args: []string{
+								"-caPath=/etc/app/certs/onf.cacrt",
+								"-keyPath=/etc/app/certs/onos-config.key",
+								"-certPath=/etc/app/certs/onos-config.crt",
+							},
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "grpc",
+									ContainerPort: 5150,
+								},
+								{
+									Name:          "debug",
+									ContainerPort: 40000,
+									Protocol:      corev1.ProtocolTCP,
+								},
+							},
+							ReadinessProbe: &corev1.Probe{
+								Handler: corev1.Handler{
+									TCPSocket: &corev1.TCPSocketAction{
+										Port: intstr.FromInt(5150),
+									},
+								},
+								InitialDelaySeconds: 5,
+								PeriodSeconds:       10,
+							},
+							LivenessProbe: &corev1.Probe{
+								Handler: corev1.Handler{
+									TCPSocket: &corev1.TCPSocketAction{
+										Port: intstr.FromInt(5150),
+									},
+								},
+								InitialDelaySeconds: 15,
+								PeriodSeconds:       20,
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "app",
+									MountPath: "/etc/app/configs",
+									ReadOnly:  true,
+								},
+								{
+									Name:      "secret",
+									MountPath: "/etc/app/certs",
+									ReadOnly:  true,
+								},
+							},
+							SecurityContext: &corev1.SecurityContext{
+								Capabilities: &corev1.Capabilities{
+									Add: []corev1.Capability{
+										"SYS_PTRACE",
+									},
+								},
+							},
+						},
+					},
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsUser: &zero,
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "app",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: name,
+									},
+								},
+							},
+						},
+						{
+							Name: "secret",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: c.clusterID,
+								},
 							},
 						},
 					},
@@ -146,8 +204,7 @@ func (c *ClusterController) createAppPod(name string) error {
 			},
 		},
 	}
-
-	_, err := c.kubeclient.CoreV1().Pods(c.clusterID).Create(pod)
+	_, err := c.kubeclient.AppsV1().Deployments(c.clusterID).Create(dep)
 	return err
 }
 
@@ -176,24 +233,27 @@ func (c *ClusterController) createAppService(name string) error {
 	return err
 }
 
-// awaitAppReady waits for the given app to complete startup
-func (c *ClusterController) awaitAppReady(name string) error {
+// awaitOnosAppDeploymentReady waits for the app pods to complete startup
+func (c *ClusterController) awaitOnosAppDeploymentReady(name string) error {
 	for {
-		pod, err := c.kubeclient.CoreV1().Pods(c.clusterID).Get(name, metav1.GetOptions{})
+		// Get the app deployment
+		dep, err := c.kubeclient.AppsV1().Deployments(c.clusterID).Get(name, metav1.GetOptions{})
 		if err != nil {
 			return err
-		} else if len(pod.Status.ContainerStatuses) > 0 && pod.Status.ContainerStatuses[0].Ready {
-			return nil
-		} else {
-			time.Sleep(100 * time.Millisecond)
 		}
+
+		// Return once the all replicas in the deployment are ready
+		if int(dep.Status.ReadyReplicas) == c.config.ConfigNodes {
+			return nil
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
 }
 
 // teardownApp tears down a app by name
 func (c *ClusterController) teardownApp(name string) error {
 	var err error
-	if e := c.deleteAppPod(name); e != nil {
+	if e := c.deleteAppDeployment(name); e != nil {
 		err = e
 	}
 	if e := c.deleteAppService(name); e != nil {
@@ -211,8 +271,8 @@ func (c *ClusterController) deleteAppConfigMap(name string) error {
 }
 
 // deleteAppPod deletes an app Pod by name
-func (c *ClusterController) deleteAppPod(name string) error {
-	return c.kubeclient.CoreV1().Pods(c.clusterID).Delete(name, &metav1.DeleteOptions{})
+func (c *ClusterController) deleteAppDeployment(name string) error {
+	return c.kubeclient.AppsV1().Deployments(c.clusterID).Delete(name, &metav1.DeleteOptions{})
 }
 
 // deleteAppService deletes an app Service by name
