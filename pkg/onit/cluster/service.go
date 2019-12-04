9a196d749c6d723c93ac1fb812df508daacf8fd8
Jordan Halterman <jordan.halterman@gmail.com>
2019-12-04 10:18:22 -0800

    Ensure setup awaits services when default replicas is 0

--- pkg/onit/cluster/service.go
+++ pkg/onit/cluster/service.go
@@ -440,7 +440,7 @@ func (s *Service) createDeployment() error {
 
 // AwaitReady waits for the service to complete startup
 func (s *Service) AwaitReady() error {
-	if s.replicas == 0 {
+	if s.Replicas() == 0 {
 		return nil
 	}
 
