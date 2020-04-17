2576e2ee63f27de59c729cdf819a6353f9a7e0bc
Ray Milkey <ray@opennetworking.org>
2020-04-17 09:38:52 -0700

    Remove onit files - it is superseded by helmit

--- build/bin/compile_protos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-proto_imports="./pkg:${GOPATH}/src/github.com/gogo/protobuf:${GOPATH}/src/github.com/gogo/protobuf/protobuf:${GOPATH}/src"
-
-protoc -I=$proto_imports --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,import_path=onos/benchmark,plugins=grpc:pkg pkg/benchmark/*.proto
-protoc -I=$proto_imports --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,import_path=onos/model,plugins=grpc:pkg pkg/model/*.proto
-protoc -I=$proto_imports --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mmodel/model.proto=github.com/onosproject/onos-test/pkg/model,import_path=onos/simulation,plugins=grpc:pkg pkg/simulation/*.proto
