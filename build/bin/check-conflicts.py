ccd4d95d99ed9bb924bf841ae8c15ccda553f0c7
Francis Chua <francis@opennetworking.org>
2021-07-07 13:18:20 -0700

    License fix?

--- build/bin/check-conflicts.py
+++ build/bin/check-conflicts.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
-""""
- Copyright 2021-present Open Networking Foundation.
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-
- Reads the standard input for CLI command output from 'onos pco listall pci'
- and checks that the output is correct.
-"""
+# Copyright 2021-present Open Networking Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# CLI checker utility functions
 
 # using exact matches b/c would otherwise need different of input (pci's need to be exact, hard to generalize)
-# based off of model.yaml in ran simulator that is automatically loaded
+# values based off of model.yaml in sdran-helm-charts/ran-simulator/files/model that is automatically loaded
 
 import sys
 import time
